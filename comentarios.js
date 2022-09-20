51ecdaff69ab434bc15117f4be8fb91ce22457ab
Marconi <marconifrancisco@live.com>
2022-09-20 18:53:49 -0300

    Introdução ao JS 20-09

---
+++ comentarios.js
@@ -0,0 +1,10 @@
+//  Comentário
+
+//  O código abaixo escreve alguma mensagem no devtools
+
+/*  Comentário 
+    MultiLinha*/
+
+console.log('Bem vindos ao Starter')
+console.log('Não irá ignorar')
+
---
+++ scripts.js
@@ -0,0 +1,5 @@
+console.log("'Usando aspas simples dentro de aspas duplas'")
+console.log('"Usando aspas duplas dentro de aspas simples"')
+console.log(`Usando crases 
+                          em multi linhas`)
+console.log(`Resultado de 1 + 1:  ${1 + 1}`)                         
