0307206a6aeb4e1ec83f2295494cad6b73767ed1
Marconi <marconifrancisco@live.com>
2022-09-21 22:23:24 -0300

    JS 21-09

---
+++ functions.js
@@ -0,0 +1,20 @@
+//  APLICATIVOS DE FRASES
+
+
+//  DECLARATION
+
+function createPhrases(){
+    console.log("Estudar é muito bom!")
+    console.log("Paciência e persistência")
+    console.log("Revisão é muito importante")
+
+}
+
+//  EXECUTE, RUN, CALL, INVOKE
+
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+
+console.log("End")
